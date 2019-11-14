ab4600c40ed9b980f64746e0f4b0a358ff0ae7e9
grantkee <grant.d.kee@gmail.com>
2019-11-14 14:23:57 -0600

    working on reorganizing my specific car information into chips that display within a paper container. mostly just focused on styling at this point. everything is functioning.

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
-import { Container, Paper, Chip } from '@material-ui/core'
+import { Container, Paper, Chip, Typography, CssBaseline } from '@material-ui/core'
+// import Typography from '@material-ui/core/Typography'
 
 const Car = (props) => {
     let id = props.match.params.id;
     let specificCar = cars.find(car => id == car.id)
     return (
-        <Container>
-        <div>
-            <h1>{specificCar.Name}</h1>
-            <p>{specificCar.id}</p>
-        </div>
-        </Container>
+
+        <Paper>
+        <Typography variant="h5" component="h3">
+            {specificCar.Name}
+        </Typography>
+        <Typography component="p">
+            <Chip><br/>Miles per Gallon: {specificCar.Miles_per_Gallon}</Chip><br/>
+            Cylinders: {specificCar.Cylinders}<br/>
+            Displacement: {specificCar.Displacement}<br/>
+            Horsepower: {specificCar.Horsepower}<br/>
+            Weight in lbs: {specificCar.Weight_in_lbs}<br/>
+            Acceleration: {specificCar.Acceleration}<br/>
+            Year: {specificCar.Year}<br/>
+            Origin: {specificCar.Origin}<br/>        
+        </Typography>
+        </Paper>
+    //         <React.Fragment>
+    //             <CssBaseline />
+    //             <Container maxWidth="sm">
+    //                 <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
+    //                 <h1>{specificCar.Name}</h1>
+    //                 <p>
+    //                     Miles per Gallon: {specificCar.Miles_per_Gallon}<br/>
+    //                     Cylinders: {specificCar.Cylinders}<br/>
+    //                     Displacement: {specificCar.Displacement}<br/>
+    //                     Horsepower: {specificCar.Horsepower}<br/>
+    //                     Weight in lbs: {specificCar.Weight_in_lbs}<br/>
+    //                     Acceleration: {specificCar.Acceleration}<br/>
+    //                     Year: {specificCar.Year}<br/>
+    //                     Origin: {specificCar.Origin}<br/>
+    //                 </p>
+    //                 </Typography>
+    //            </Container>
+    // </React.Fragment>
     )
 }
 
