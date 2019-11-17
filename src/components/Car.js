4a84e75d656d7c5e34ea0ef866dc20bd28b5b718
grantkee <grant.d.kee@gmail.com>
2019-11-16 19:05:38 -0600

    finalfinal - changed the way I display information of the specific car. now using a map function to display information

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
-import { Container, Paper, Chip, Typography, CssBaseline } from '@material-ui/core'
+import { Container, Paper, Chip } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles';
 // import Typography from '@material-ui/core/Typography'
 
-//
 const Car = (props) => {
     let id = props.match.params.id;
     let specificCar = cars.find(car => id === `${car.id}`)
@@ -18,21 +17,16 @@ const Car = (props) => {
     }))
     
     return (
-        <Paper style={{margin:'40px auto auto auto', width: '400px'}}>
-        <Typography variant="h5" component="h3" style={{margin: '15px'}}>
-            {specificCar.Name}
-        </Typography>
-        <Typography component="p" style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', height: '300px', width: '400px'}}>
-            <br/><Chip label={`Miles per Gallon: ${specificCar.Miles_per_Gallon}`}></Chip><br/>
-            <Chip label={`Cylinders: ${specificCar.Cylinders}`}/><br/>
-            <Chip label={`Displacement: ${specificCar.Displacement}`}/><br/>
-            <Chip label={`Horsepower: ${specificCar.Horsepower}`}/><br/>
-            <Chip label={`Weight in lbs: ${specificCar.Weight_in_lbs}`}/><br/>
-            <Chip label={`Acceleration: ${specificCar.Acceleration}`}/><br/>
-            <Chip label={`Year: ${specificCar.Year}`}/><br/>
-            <Chip label={`Origin: ${specificCar.Origin}`}/><br/>        
-        </Typography>
-        </Paper>
+        <Container maxWidth="sm" className={useStyles.root}>
+            <Paper >
+                <h1>{specificCar.Name}</h1>
+                {
+                    Object.keys(specificCar).map((info) => {
+                        return <Chip label={`${info}: ${specificCar[info]}`}/>
+                    })
+                }
+            </Paper>
+        </Container>
     )
 }
 
